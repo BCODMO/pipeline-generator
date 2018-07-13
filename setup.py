becc051985e452128090f9c167db56aff620f706
Conrad <conrad.schloer@gmail.com>
2018-07-13 15:20:46 -0400

    Bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 required = []
 
 setup(
-    name='bpvalve',
+    name='pipeline-generator',
     version='v0.0.1dev',
     description='BCODMO Pipelines Library',
     author='BCODMO',
-    author_email='admin@blocpower.org',
-    url='https://github.com/Blocp/bpvalve',
+    author_email='cschloer@whoi.edu',
+    url='https://github.com/bcodmo/pipeline-generator',
     packages=find_packages(),
     install_requires=required,
     include_package_data=True,
