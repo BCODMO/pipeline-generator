015e0038fb865c413a4e8b31fcc09e06e682c673
Conrad Schloer <conrad.schloer@gmail.com>
2019-10-18 12:26:09 -0400

    Bump version of bcodmo_processor

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ required = [
     'PyYAML==5.1',
     'virtualenv==16.7.5',
     'psutil==5.6.3',
-    'bcodmo_processors @ git+https://git@github.com/BCODMO/bcodmo_processors.git@master',
+    'bcodmo_processors @ git+https://git@github.com/BCODMO/bcodmo_processors.git@v1.0.4',
 ]
 
 
