abf06224d0ccb90490f4fb716517c79d07e41e04
Conrad Schloer <conrad.schloer@gmail.com>
2019-11-11 10:30:59 +0100

    Bump requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ required = [
     'PyYAML==5.1',
     'virtualenv==16.7.5',
     'psutil==5.6.3',
-    'bcodmo_processors @ git+https://git@github.com/BCODMO/bcodmo_processors.git@master',
+    'bcodmo_processors @ git+https://git@github.com/BCODMO/bcodmo_processors.git@v1.0.5',
 ]
 
 
 setup(
     name='pipeline-generator',
-    version='v0.0.4e',
+    version='v1.0.5',
     description='BCODMO Pipelines Library',
     author='BCODMO',
     author_email='conrad.schloer@gmail.com',
